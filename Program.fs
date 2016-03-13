fde4894757035d3b36d2950f7a82a91fde947f2b
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-03-13 17:36:38 -0400

    first commit

---
+++ Program.fs
@@ -0,0 +1,32 @@
+﻿open System
+open System.IO
+
+let printCubes min max =
+    let cube n =
+        n*n*n
+    [|min..max|]
+    |> Array.map cube
+    |> Array.iter (printfn "%i")
+printCubes 0 5
+
+let arr = [|0..5|]
+printfn "%A" arr
+arr.[3] <- 99
+printfn "%A" arr //You can also inspect the array in Fsharpi by typing 'arr;;'
+
+let lis = [0..5]
+printfn "%A" lis
+
+let printCubes2 min max =
+    let cube n =
+        n*n*n
+    [min..max]
+    |> List.map cube
+    |> List.iter (printfn "%i")
+printCubes2 0 5
+
+[<EntryPoint>]
+let main argv = 
+    //printfn "%A" argv
+    0 // return an integer exit code
+
